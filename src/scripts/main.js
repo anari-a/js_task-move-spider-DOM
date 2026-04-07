8b7edd772a48659eda3f34de41e3f0dd4f9de562
Anastasiia Riadnova <a.ryadnova01@gmail.com>
2026-04-07 18:25:46 +0200

    add task solution v2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,17 +4,14 @@ const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
 wall.addEventListener('click', (e) => {
-  const wallRect = wall.getBoundingClientRect();
-  const spiderRek = spider.getBoundingClientRect();
+  const relX = e.clientX - wall.offsetLeft - wall.clientLeft;
+  const relY = e.clientY - wall.offsetTop - wall.clientTop;
 
-  const relX = e.clientX - wallRect.left;
-  const relY = e.clientY - wallRect.top;
+  let left = relX - spider.offsetWidth / 2;
+  let topy = relY - spider.offsetHeight / 2;
 
-  let left = relX - spiderRek.width / 2;
-  let topy = relY - spiderRek.height / 2;
-
-  left = Math.max(0, Math.min(left, wallRect.width - spiderRek.width));
-  topy = Math.max(0, Math.min(topy, wallRect.height - spiderRek.height));
+  left = Math.max(0, Math.min(left, wall.clientWidth - spider.offsetWidth));
+  topy = Math.max(0, Math.min(topy, wall.clientHeight - spider.offsetHeight));
 
   spider.style.left = left + 'px';
   spider.style.top = topy + 'px';
